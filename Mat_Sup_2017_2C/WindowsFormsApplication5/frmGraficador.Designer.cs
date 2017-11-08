223b1ed2c7f375ba8c60fef7543447dedbc1f9c0
JL <JLSR@users.noreply.github.com>
2017-11-08 14:34:47 -0300

    Agregado el frm q solo muestra el grafico

---
+++ Mat_Sup_2017_2C/WindowsFormsApplication5/frmGraficador.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TP_MateSuperior
+{
+    partial class frmGraficador
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, 1D);
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.BackColor = System.Drawing.SystemColors.Control;
+            chartArea1.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisX2.TitleForeColor = System.Drawing.Color.White;
+            chartArea1.BackColor = System.Drawing.SystemColors.Control;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(3, 2);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.LabelBackColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Puntos";
+            dataPoint1.LabelBackColor = System.Drawing.Color.Red;
+            series1.Points.Add(dataPoint1);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Recta";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Parabola";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Exponencial";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "Potencial";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Hiperbola";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.IsVisibleInLegend = false;
+            series7.Legend = "Legend1";
+            series7.Name = "Hiperbola2";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Series.Add(series3);
+            this.chart.Series.Add(series4);
+            this.chart.Series.Add(series5);
+            this.chart.Series.Add(series6);
+            this.chart.Series.Add(series7);
+            this.chart.Size = new System.Drawing.Size(748, 559);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            this.chart.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // frmGraficador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 559);
+            this.Controls.Add(this.chart);
+            this.MaximizeBox = false;
+            this.Name = "frmGraficador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TP Matematica Superior";
+            this.Load += new System.EventHandler(this.frmGraficador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart;
+
+
+    }
+}
+
---
+++ Mat_Sup_2017_2C/WindowsFormsApplication5/frmGraficador.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_MateSuperior
+{
+    public partial class frmGraficador : Form
+    {
+        double x, y;
+        //double a, b, c;
+
+        public frmGraficador()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }              
+             
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void borrarGraficos()
+        {
+            chart.Series[0].Enabled = false;
+            chart.Series[1].Enabled = false;
+            chart.Series[2].Enabled = false;
+            chart.Series[3].Enabled = false;
+            chart.Series[4].Enabled = false;
+            chart.Series[5].Enabled = false;
+
+            //chart.Series[6].Enabled = false; 
+        }
+
+        
+        public void graficarPuntos()
+        {
+            //
+            chart.Series[0].Enabled = true;
+        }
+
+        public void graficarRecta(double a, double b, double cotaMin, double cotaMax) 
+        {
+            chart.Series[1].Points.Clear();
+            borrarGraficos();
+
+            for (x = cotaMin; x <= cotaMax; x += 0.5)
+            {
+                y = (a * x) + b;
+                chart.Series[1].Points.AddXY(x, y);
+            }
+            chart.Series[1].Enabled = true;
+ 
+        }
+
+        private void rbRecta_Leave(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        
+        public void graficarParabola(double a, double b, double c, double cotaMin, double cotaMax)
+        {
+            chart.Series[2].Points.Clear();
+            borrarGraficos();
+
+            for (x = cotaMin; x <= cotaMax; x += 0.5)
+            {
+                y = (a * x * x) + (b * x) + c;
+                chart.Series[2].Points.AddXY(x, y);
+            }
+            chart.Series[2].Enabled = true;
+        }
+        
+        public void graficarExponencial(double a, double b, double cotaMin, double cotaMax)
+        {
+            chart.Series[3].Points.Clear();
+            borrarGraficos();
+
+            for (x = cotaMin; x <= cotaMax; x += 0.5)
+            {
+                //y = b * (Math.Pow(Math.E,(a*x)));
+                y = b * Math.Exp(a * x);
+                chart.Series[3].Points.AddXY(x, y);
+            }
+            chart.Series[3].Enabled = true;
+        }
+               
+        public void graficarPotencial(double a, double b, double cotaMin, double cotaMax)
+        {
+            chart.Series[4].Points.Clear();
+            borrarGraficos();
+
+            for (x = cotaMin; x <= cotaMax; x += 0.5)
+            {
+                y = b * Math.Pow(x,a);
+                chart.Series[4].Points.AddXY(x, y);
+            }
+            chart.Series[4].Enabled = true;
+        }
+                
+        public void graficarHiperbola(double a, double b, double cotaMin, double cotaMax)
+        {
+            double asintota;
+
+            chart.Series[5].Points.Clear();
+            chart.Series[6].Points.Clear();
+            borrarGraficos();
+            
+            asintota = -b;
+            
+            for (x = cotaMin; x <= asintota - 0.1; x += 0.5)
+            {
+                y = a / (x + b);
+                chart.Series[5].Points.AddXY(x, y);                
+            }
+
+            for (x = asintota + 0.5; x <= cotaMax; x += 0.5)
+            {
+                y = a / (x + b);
+                chart.Series[6].Points.AddXY(x, y);
+            }
+
+            chart.Series[5].Color = Color.Green;
+            chart.Series[5].Enabled = true;
+            chart.Series[6].Color = Color.Green;
+            chart.Series[6].Enabled = true;
+        }
+
+        private void frmGraficador_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
